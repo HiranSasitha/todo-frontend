2141c3eed46eed80c814ce3ba89faef53b0f9e92
HiranSasitha <hiransasitha123@gamil.com>
2024-04-01 00:19:50 +0530

    feat(api): completed user register

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import Home from './Home';
 import AddTodo from './AddTodo';
 import UpdateTaskItem from './UpdateTaskItem';
 import CompletedTask from './CompletedTask';
+import RegisterUser from './auth/RegisterUser';
 
 
 function App() {
@@ -14,7 +15,8 @@ function App() {
     <div className="App">
     <BrowserRouter>
     <Routes>
-    <Route index element ={<Home/>}/>
+    <Route index element ={<RegisterUser/>}/>
+    <Route path='/home' element ={<Home/>}/>
     <Route path='/add-todo' element ={<AddTodo/>}/>
     <Route path='/update-todo/:id' element ={<UpdateTaskItem/>}/>
     <Route path='/completed-todo' element ={<CompletedTask/>}/>
--- src/Header.tsx
+++ src/Header.tsx
@@ -23,7 +23,7 @@ const Header = () => {
           <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav me-3">
               <li className="nav-item">
-                <Link className="nav-link" to={"/"}>Home</Link>
+                <Link className="nav-link" to={"/home"}>Home</Link>
               </li>
               <li className="nav-item">
                 <Link className="nav-link" to={"/completed-todo"}>Completed Task</Link>
